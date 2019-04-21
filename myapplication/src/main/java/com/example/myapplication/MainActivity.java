6d5b1d73a77eb57a6a1d17914e2b980915a9ac76
Apple <>
2019-04-21 15:01:10 +0900

    terai2

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("test");
+
         setContentView(R.layout.activity_main);
     }
 }
